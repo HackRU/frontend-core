644ab655b71dd6d16afd3054488a3672035a81a9
Gabriel Weinbrenner <angelgabe101@gmail.com>
2020-12-20 21:42:13 -0500

    intial

---
+++ example/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Hello from 'npm-react-components-boilerplate';
+function App() {
+  return (
+    <div className="App">
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import sass from 'rollup-plugin-sass'
+import jsx from 'rollup-plugin-jsx'
+
+import pkg from './package.json'
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
+      strict: false
+    }
+  ],
+  plugins: [sass({ insert: true }), jsx({factory: "React.createElement"})],
+  external: ['react', 'react-dom']
+}
