6803f00b5c974b694a27bfa19465a0149c9f82b3
shivanmodha <shivanmodha@gmail.com>
2021-01-03 16:51:22 -0500

    To babel v7
     - Updated dependencies to babel 7\n - Removed all typescript references\n - Added MIT licenses\n - Cleaned up node dependencies\n - Defined package name

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,30 @@
-import sass from 'rollup-plugin-sass'
-import jsx from 'rollup-plugin-jsx'
+import sass from "rollup-plugin-sass";
+import babel from "rollup-plugin-babel";
 
-import pkg from './package.json'
+import pkg from "./package.json";
 
 export default {
-  input: 'src/index.js',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: true,
-      strict: false
-    }
-  ],
-  plugins: [sass({ insert: true }), jsx({factory: "React.createElement"})],
-  external: ['react', 'react-dom']
+    input: "src/index.js",
+    output: [{
+        file: pkg.main,
+        format: "cjs",
+        exports: "named",
+        sourcemap: true,
+        strict: false
+    }],
+    plugins: [
+        babel({
+            exclude: "node_modules/**",
+            presets: [
+                ["@babel/preset-react", {modules: false}]
+            ] 
+        }),
+        sass({
+            insert: true
+        })
+    ],
+    external: [
+        "react",
+        "react-dom"
+    ]
 }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
-import React, {useState} from 'react';
-function getRandColor(){
-  const r = Math.floor(Math.random() * 16777215).toString(16);
-  const g = Math.floor(Math.random() * 16777215).toString(16)
-  const b = Math.floor(Math.random() * 16777215).toString(16)
-  return `#${r}${g}${b}`
-}
-function hello(){
-  // [color, setColor] = useState('#000000');
-  return(
-    // <div onClick={() => setColor(getRandColor())}>
-      // <h1 style={{color: color}}>Hello</h1>
-    // </div>
-    <div>
-      <h1>Hello</h1>
-    </div>
-  )
-}
+import React from "react";
 
+const Main = () => (
+    <>
+        Testing
+    </>
+)
 
-export default hello;
+export default Main;
