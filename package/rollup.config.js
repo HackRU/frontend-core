713a8fdaf9bed7b1607b6b422a6a4fe1793bb15b
Zain Ali <zain08816@gmail.com>
2021-03-25 12:43:37 -0600

    Update rollup config

--- package/rollup.config.js
+++ package/rollup.config.js
@@ -1,28 +1,64 @@
-import scss from "rollup-plugin-scss";
+// import scss from "rollup-plugin-scss";
 import babel from "rollup-plugin-babel";
+import resolve from 'rollup-plugin-node-resolve';
 
 import pkg from "./package.json";
 
-export default {
-    input: "src/index.js",
-    output: [{
-        file: pkg.main,
-        format: "cjs",
-        exports: "named",
-        sourcemap: true,
-        strict: false
-    }],
-    plugins: [
-        babel({
-            exclude: "node_modules/**",
-            presets: [
-                ["@babel/preset-react", {modules: false}]
+// export default 
+//     [
+//         {
+//             input: "src/index.js",
+//             output: [{
+//                 file: pkg.main+"/index.js",
+//                 format: "cjs",
+//                 exports: "named",
+//                 sourcemap: true,
+//                 strict: false
+//             }],
+//             plugins: [
+//                 babel({
+//                     exclude: "node_modules/**",
+//                     presets: [
+//                         ["@babel/preset-react", {modules: false}]
+//                     ]
+//                 }),
+//                 scss()
+//             ],
+//             external: [
+//                 "react",
+//                 "react-dom"
+//             ]
+
+//         },
+//     ]
+    
+// ;
+
+export default 
+    [
+        {
+            input: "src/index.js",
+            output: {
+                file: pkg.main,
+                format: "cjs"
+            },
+            external: [
+                'react',
+                'react-proptypes',
+                'Profile',
+                'Defaults',
+                'prop-types',
+            ],
+            plugins: [
+                resolve(),
+                babel({
+                    exclude: 'node_modules/**',
+                    presets: [
+                        ["@babel/preset-react", {modules: false}]
+                    ]
+                })
             ]
-        }),
-        scss()
-    ],
-    external: [
-        "react",
-        "react-dom"
+        },
     ]
-};
+    
+;
